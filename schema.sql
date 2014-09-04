b6501cc70556de46cffbeca11e42c5ad59a4b9c1
TraceHagan <trace_hagan@yahoo.com>
2014-09-04 15:18:10 -0700

    updated db schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS duplicates;
 DROP TABLE IF EXISTS files;
+DROP TABLE IF EXISTS signatures;
+
 
 CREATE TABLE files
 (
@@ -17,4 +19,10 @@ CREATE TABLE duplicates
  file_hash VARCHAR(128),
  FOREIGN KEY (dupe1_id) REFERENCES files(record_id),
  FOREIGN KEY (dupe2_id) REFERENCES files(record_id)
+);
+
+CREATE TABLE signatures
+(
+ record_id BIGINT(19) PRIMARY KEY AUTO_INCREMENT,
+ file_hash VARCHAR(128)
 );
