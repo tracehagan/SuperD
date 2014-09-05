e2945798f9101c4a5a4bde08b3a71fec06b83b01
TraceHagan <trace_hagan@yahoo.com>
2014-09-04 21:50:20 -0700

    finds duplicates. makes report deliniated by |~|~|

--- src/DupesReport/Main.java
+++ src/DupesReport/Main.java
@@ -26,40 +26,7 @@ public class Main {
         log.info("Starting program!");
         H2 db = null;
 
-        /*try{
-            db = Database.getInstance();
-            try {
-                db.openConnection();
-                log.debug("Opened connection");
-            } catch (ClassNotFoundException | SQLException e) {
-                log.fatal("Failed to open database connection! Check config file!", e);
-            }
-            String sqlReport = "SELECT file_path, file_size FROM files INNER JOIN duplicates ON files.record_id = duplicates.dupe1_id ORDER BY file_size DESC";
-            PreparedStatement psReport = null;
-            ResultSet rsReport = null;
-            try {
-                psReport = db.getConnection().prepareStatement(sqlReport);
-            } catch (Exception e){
-                e.printStackTrace();
-            }
-            try {
-                rsReport = psReport.executeQuery();
-                log.debug("executed query");
-                PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream("report.txt"), "utf-8")));
-                //TODO MAKE REPORT
 
-                while (rsReport != null && rsReport.next()){
-                    log.debug(rsReport.getString("file_size"));
-                    writer.write(rsReport.getString("file_path") + " | " + rsReport.getString("file_size") + "\n");
-
-                }
-                writer.close();
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }*/
 
         try{
             db = Database.getInstance();
@@ -69,53 +36,52 @@ public class Main {
             } catch (ClassNotFoundException | SQLException e) {
                 log.fatal("Failed to open database connection! Check config file!", e);
             }
-            String sqlReport = "SELECT file_hash FROM signatures;";
-            String sqlCount = "SELECT count(*) FROM files;";
-            String sqlNonUnique = "SELECT COUNT(DISTINCT file_hash) FROM files;";
+            String sqlHashes = "SELECT file_hash FROM signatures;";
+            String sqlDuplicates = "SELECT file_path FROM nonUnique WHERE file_hash = ?";
             String sqlHashCount = "SELECT count(*) FROM signatures;";
-            String sqlNumRows = "SELECT count(*) from nonUnique;";
-            PreparedStatement psCount = null;
+
             PreparedStatement psHashCount = null;
-            PreparedStatement psReport = null;
-            PreparedStatement psNonUnique = null;
-            PreparedStatement psNumRows = null;
-            ResultSet rsNumRows = null;
-            ResultSet rsReport = null;
-            ResultSet rsCount = null;
+            PreparedStatement psHashes = null;
+            PreparedStatement psDuplicates = null;
+
+            ResultSet rsHashes = null;
             ResultSet rsHashCount = null;
-            ResultSet rsNonUnique = null;
+            ResultSet rsDuplicates = null;
+
             try {
-                psReport = db.getConnection().prepareStatement(sqlReport);
-                psCount = db.getConnection().prepareStatement(sqlCount);
+                psHashes = db.getConnection().prepareStatement(sqlHashes);
+                psDuplicates = db.getConnection().prepareStatement(sqlDuplicates);
                 psHashCount = db.getConnection().prepareStatement(sqlHashCount);
-                psNonUnique = db.getConnection().prepareStatement(sqlNonUnique);
-                psNumRows = db.getConnection().prepareStatement(sqlNumRows);
+
             } catch (Exception e){
                 e.printStackTrace();
             }
+            int numSig=0;
             try {
-                rsCount = psCount.executeQuery();
-                rsCount.next();
-                log.debug(rsCount.getInt(1));
                 rsHashCount = psHashCount.executeQuery();
                 rsHashCount.next();
-                log.debug(rsHashCount.getInt(1));
-                log.debug("num unique");
-                rsNonUnique = psNonUnique.executeQuery();
-                rsNonUnique.next();
-                log.debug(rsNonUnique.getInt(1));
-                log.debug("num rows");
-                rsNumRows = psNumRows.executeQuery();
-                rsNumRows.next();
-                log.debug(rsNumRows.getInt(1));
-                rsReport = psReport.executeQuery();
+                numSig = rsHashCount.getInt(1);
+
+                rsHashes = psHashes.executeQuery();
                 log.debug("executed query");
+
                 PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream("report.txt"), "utf-8")));
                 //TODO MAKE REPORT
 
-                while (rsReport != null && rsReport.next()){
+                while (rsHashes != null && rsHashes.next()){
                     //log.debug(rsReport.getString("file_hash"));
-                    writer.write(rsReport.getString("file_hash") + "\n");
+                    psDuplicates.setString(1, rsHashes.getString(1));
+                    rsDuplicates = psDuplicates.executeQuery();
+                    rsDuplicates.next();
+                    writer.write(rsDuplicates.getString("file_path"));
+                    while (rsDuplicates != null && rsDuplicates.next()){
+                        writer.write("|~|~|" + rsDuplicates.getString("file_path"));
+                    }
+                    writer.write("\n");
+                    psDuplicates.clearParameters();
+
+                    rsDuplicates.close();
+                    rsDuplicates = null;
 
                 }
                 writer.close();
